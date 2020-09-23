42a3a16b7abf0ec915441c453a56a97246f20c0d
lgss1 <1773204026@qq.com>
2020-09-23 09:54:18 +0800

    完善

--- Bench_user/src/main/java/com/bench/Bench/BenchUserApplication.java
+++ Bench_user/src/main/java/com/bench/Bench/BenchUserApplication.java
@@ -12,6 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 public class BenchUserApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(BenchUserApplication.class, args);
     }
